13b0b2ef9327cf04ae6591b641e8d3945f0a3ca3
hansyao <hansyow@gmail.com>
2021-07-21 17:57:46 +0800

    add clash pool

--- VmessActions/pool.sh
+++ VmessActions/pool.sh
@@ -2,17 +2,27 @@
 
 URL=https://proxy.yugogo.xyz/clash/proxies
 TEMP=VmessActions/subscribe/pool.yaml
+CLASH=VmessActions/subscribe/clash_pool.yaml
 
 rm -f $TEMP
 i=1
 while [[ $(cat $TEMP | sed -n '1p' | sed s/\ //g) != "proxies:" ]]
 do
-	sleep 1
+	if [ $i -ge 500 ]; then
+                break
+        fi
+                
+        sleep 1
 	echo -e 第 $i 次爬取失败
 	rm -f $TEMP
 	curl -s $URL > $TEMP
 	let i++
 done
-
-echo -e "第 $i 次爬取成功 获得节点信息 >> $TEMP"
+if [ $i -lt 500 ]; then
+        echo -e "第 $i 次爬取成功 获得节点信息 >> $TEMP"
+        docker run -d --restart=always -p 25500:25500 tindy2013/subconverter:latest
+        curl -s http://127.0.0.1:25500/sub\?target\=clash\&emoji\=true\&url\=https%3A%2F%2Fraw.githubusercontent.com%2Fhansyao%2Fbreakwall%2Fmaster%2FVmessActions%2Fsubscribe%2Fpool.yaml > $CLASH
+else
+        echo -e "爬取失败!"
+fi
 
