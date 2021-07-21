6e7c5abcfe433474e3f7702daffc0325f7c7e2ba
hansyao <hansyow@gmail.com>
2021-07-21 18:57:43 +0800

    update clash pool

--- VmessActions/pool.sh
+++ VmessActions/pool.sh
@@ -21,7 +21,7 @@ done
 if [ $i -lt 500 ]; then
         echo -e "第 $i 次爬取成功 获得节点信息 >> $TEMP"
         echo -e "开始规则转换"
-        curl -s http://127.0.0.1:25500/sub\?target\=clash\&emoji\=true\%url\=VmessActions%2Fsubscribe%2Fpool.yaml> $CLASH
+        curl -s http://127.0.0.1:25500/sub\?target\=clash\&emoji\=true\&url\=VmessActions%2Fsubscribe%2Fpool.yaml >$CLASH
         echo -e "clash规则转化完成"
 else
         echo -e "爬取失败!"
