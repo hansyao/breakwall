98ec59af1d3041a0d5c4f321af1a76d823110deb
hansyao <hansyow@gmail.com>
2021-07-24 10:43:54 +0800

    update converter

--- VmessActions/convert.sh
+++ VmessActions/convert.sh
@@ -18,9 +18,14 @@ VERSION=$(get_latest_release $REPO)
 rm -rf $APP
 rm -f $FILE
 
-wget $GITHUB/$REPO/releases/download/$VERSION/$APP\_$OS.tar.gz -O $FILE
-tar -xvf $FILE
+echo -e "下载$FILE"
+curl -s -L -X GET $GITHUB/$REPO/releases/download/$VERSION/$APP\_$OS.tar.gz -o $FILE
+echo -e "下载完成, 开始解压缩到`pwd`/$APP"
+tar -xvf $FILE >/dev/null 2>&1
+echo -e "删除$FILE"
 rm -f $FILE
 
+echo -e "设置权限 chmod 755 $APP/$APP"
 chmod 755 $APP/$APP
+echo -e "运行规则转换工具"
 $APP/$APP >/dev/null 2>&1 &
