ab8aede18feb8155cb697499a1f6314147925b8a
hansyao <hansyow@gmail.com>
2021-07-29 03:15:30 +0800

    update multi-thread

--- VmessActions/proxy_rename.sh
+++ VmessActions/proxy_rename.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#POOL=VmessActions/subscribe/latest_pool.yaml
-#FINAL_POOL=VmessActions/subscribe/pool.yaml
+POOL=VmessActions/subscribe/latest_pool.yaml
+FINAL_POOL=VmessActions/subscribe/pool.yaml
 
-POOL=$1
-FINAL_POOL=$2
+#POOL=$1
+#FINAL_POOL=$2
 LOCATION=VmessActions/location.txt
 
 
@@ -173,13 +173,15 @@ pool_rename() {
 }
 
 
-# 代理节点重命名 -- 多线程
+# 代理节点重命名 -- 多线程 -- 堵塞
 # $1 - 输出配置文件 $2 - 每个进程最大处理行数设定 
-multi_pool_rename() {
-
+multi_pool_rename_pid() {
 	TOTAL=$(cat $POOL | wc -l)
 	m=$2
 	n=$(expr $TOTAL / $m + 1)
+
+	START_TIME=$(date +%s)
+
 
 	TEMP=tmp
 	rm -rf $TEMP
@@ -208,13 +210,83 @@ multi_pool_rename() {
 	done
 
 	rm -rf $TEMP
+	
+	STOP_TIME=$(date +%s)
+	echo -e "总耗时: `expr $[STOP_TIME] - $[START_TIME]` 秒"
+
+}
+
+
+# 代理节点重命名 -- 多线程 -- 队列非堵塞
+# $1 - 源配置文件 $2 - 目标配置文件 $3 - 每个线程队列数最大处理数
+multi_pool_rename_fd() {
+	m=$3
+
+	START_TIME=$(date +%s)
+	[ -e /tmp/fd1 ] || mkfifo /tmp/fd1
+	exec 3<>/tmp/fd1
+	rm -rf /tmp/fd1
+	for ((i=1; i<=$m; i++))
+	do
+		echo >&3
+	done
+
+	i=0
+	cat $1 | while read line || [[ -n ${line} ]]
+	do
+		read -u 3
+		let i++
+		{
+			LINE=$(echo ${line} | sed -e 's/^[ ]*//g' | sed -e 's/[ ]*$//g')
+
+			if [ $(echo $LINE | awk -F" " '{print $1}') != '-' ]; then
+				NEW_LINE=$LINE
+			else
+				SERVER=$(echo $LINE | awk -F"\"server\":" '{print $2}' \
+					| awk -F"," '{print $1}'| sed 's/\"//g')
+				COUNTRY=$(echo $LINE | awk -F"\"country\":" '{print $2}' \
+					| awk -F"," '{print $1}'| sed 's/\"//g')
+				NAME=$(echo $LINE | awk -F"\"name\":" '{print $2}' \
+					| awk -F"," '{print $1}'| sed 's/\"//g')
+
+				NEW_NAME=$(cat $LOCATION | grep -e "|${SERVER}|" \
+					| cut -d "|" -f1,3- | sed "s/\0|//g" | sed "s/|0//g")
+
+				# 修正国家代码
+				CODE=$(cat ${LOCATION} | grep -e "|${SERVER}|" | cut -d "|" -f1)
+
+				if [[ -z $(echo $LINE | grep "\"country\":") ]]; then
+					LINE=$(echo $LINE \
+						| sed -e "s/.$/,\"country\":\"$CODE\"\}/g")
+				else
+					LINE=$(echo $LINE | sed "s/\"country\":\"${COUNTRY}/\
+						\"country\":\"${CODE}/g")
+				fi
+				
+				NEW_LINE=$(echo $LINE \
+				  | sed "s/\"name\":\"${NAME}/\"name\":\"${NEW_NAME}\|$[i]\|/g")
+			fi
+
+			echo ${NEW_LINE} >> $2
+
+			echo >&3
+		}&
+	done
+	wait
+
+	STOP_TIME=$(date +%s)
+
+	echo -e "总耗时: `expr $[STOP_TIME] - $[START_TIME]` 秒"
+	exec 3<&-
+	exec 3>&-
 }
 
 
 # 得到IP地域文件
-location $POOL $LOCATION
+# location $POOL $LOCATION
 
 # 代理池按照地域改名
-multi_pool_rename $FINAL_POOL 500
+multi_pool_rename_pid $FINAL_POOL 500
 
+# multi_pool_rename_fd $POOL $FINAL_POOL 30 
 exit 0
